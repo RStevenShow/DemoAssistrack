aa21407ff51a2c2ccc13ab34d56cff793374256c
RStevenShow <ramonsteven10@yahoo.es>
2024-09-23 14:56:47 -0600

    Pantalla decano

---
+++ app/src/main/java/com/example/demoassistrack/UI/Activity_decano.kt
@@ -0,0 +1,21 @@
+package com.example.demoassistrack.UI
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.demoassistrack.R
+
+class activity_decano : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_decano)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
