05d777f5659ee307853103f0b620b5941521c35d
RStevenShow <ramonsteven10@yahoo.es>
2024-09-24 18:10:58 -0600

    Mejora diseño decanos -Frontend Estudiantes -Icon Launcher assistrack

--- app/src/main/java/com/example/demoassistrack/UI/Activity_estudiantes.kt
+++ app/src/main/java/com/example/demoassistrack/UI/Activity_estudiantes.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.demoassistrack.R
 
-class activity_estudiante : AppCompatActivity() {
+class activity_estudiantes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_estudiante)
+        setContentView(R.layout.activity_estudiantes)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
